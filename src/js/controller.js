52b51f7f1c7c07860c9a00d8274543c3cbab965a
Anna Nawrocka <anna.nawrocka1988@gmail.com>
2021-07-06 13:09:25 +0100

    Add View, SearchView and resultsView

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
 import searchView from "./views/searchView.js";
+import resultsView from "./views/resultsView.js";
 
 import "core-js/stable";
 import "regenerator-runtime/runtime";
+
+if(module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -20,21 +25,21 @@ const controlRecipes = async function () {
   }
 };
 
-const controlSearchResults = async function() {
+const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
     // Get search query
     const query = searchView.getQuery();
     if(!query) return;    
     // Load search results
     await model.loadSearchResults(query);
-    // Render results
-    console.log(model.state.search.results);
+    // Render results    
+    resultsView.render(model.state.search.results);
   } catch(err) {
     console.log(err);
   };
 };
 
-controlSearchResults();
 const init = function() {
   recipeView.addHandlerRender(controlRecipes);
   searchView.addHandlerSearch(controlSearchResults);
